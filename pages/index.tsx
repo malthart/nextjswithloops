15a74bbcaf9d0b367f9b3db6b09ab5de72fac6ca
Malthe Hartmann <malthart@icloud.com>
2023-09-10 11:38:30 +0200

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,317 @@
-import Image from 'next/image'
+import { useState } from "react";
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const INIT = "INIT";
+const SUBMITTING = "SUBMITTING";
+const ERROR = "ERROR";
+const SUCCESS = "SUCCESS";
+const formStates = [INIT, SUBMITTING, ERROR, SUCCESS] as const;
+const formStyles = {
+  "id": "clm7jxme500ewl40pcvl74azk",
+  "name": "Default",
+  "formStyle": "inline",
+  "placeholderText": "your@email.com",
+  "formFont": "Inter",
+  "formFontColor": "#000000",
+  "formFontSizePx": 14,
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  // Customize the CTA
+
+  "buttonText": "Send e-mail to Loops",
+  "buttonFont": "Inter",
+  "buttonFontColor": "#ffffff",
+  "buttonColor": "#fc5200",
+  "buttonFontSizePx": 14,
+
+  // Customize the success message
+
+  "successMessage": "E-mail was succesfully sent to Loops! Find it the Audience tab.",
+  "successFont": "Inter",
+  "successFontColor": "#000000",
+  "successFontSizePx": 14,
+  "userGroup": ""
+}
+const domain = "app.loops.so"
+
+export default function SignUpFormReact() {
+  const [email, setEmail] = useState("");
+  const [formState, setFormState] = useState<typeof formStates[number]>(INIT);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const resetForm = () => {
+    setEmail("");
+    setFormState(INIT);
+    setErrorMessage("");
+  };
+
+  /**
+   * Rate limit the number of submissions allowed
+   * @returns {boolean} true if the form has been successfully submitted in the past minute
+   */
+  const hasRecentSubmission = () => {
+    const time = new Date();
+    const timestamp = time.valueOf();
+    const previousTimestamp = localStorage.getItem("loops-form-timestamp");
+
+    // Indicate if the last sign up was less than a minute ago
+    if (
+      previousTimestamp &&
+      Number(previousTimestamp) + 60 * 1000 > timestamp
+    ) {
+      setFormState(ERROR);
+      setErrorMessage("Too many signups, please try again in a little while");
+      return true;
+    }
+
+    localStorage.setItem("loops-form-timestamp", timestamp.toString());
+    return false;
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // boundary conditions for submission
+    if (formState !== INIT) return;
+    if (!isValidEmail(email)) {
+      setFormState(ERROR);
+      setErrorMessage("Please enter a valid email");
+      return;
+    }
+    if (hasRecentSubmission()) return;
+    setFormState(SUBMITTING);
+
+    // build body
+    const formBody = `userGroup=${encodeURIComponent(
+      formStyles.userGroup
+    )}&email=${encodeURIComponent(email)}`;
+
+    // API request to add user to newsletter
+    fetch(`https://${domain}/api/newsletter-form/${formStyles.id}`, {
+      method: "POST",
+      body: formBody,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((res: any) => [res.ok, res.json(), res])
+      .then(([ok, dataPromise, res]) => {
+        if (ok) {
+          resetForm();
+          setFormState(SUCCESS);
+        } else {
+          dataPromise.then((data: any) => {
+            setFormState(ERROR);
+            setErrorMessage(data.message || res.statusText);
+            localStorage.setItem("loops-form-timestamp", "");
+          });
+        }
+      })
+      .catch((error) => {
+        setFormState(ERROR);
+        // check for cloudflare error
+        if (error.message === "Failed to fetch") {
+          setErrorMessage("Too many signups, please try again in a little while");
+        } else if (error.message) {
+          setErrorMessage(error.message);
+        }
+        localStorage.setItem("loops-form-timestamp", "");
+      });
+  };
+
+  const isInline = formStyles.formStyle === "inline";
+
+  switch (formState) {
+    case SUCCESS:
+      return (
+        <div className="flex flex-1 flex-col">
+          <div className="overflow-hidden">
+            <div className="px-4 md:px-8">
+              <div className="mx-auto w-full max-w-6xl">
+                <div className="grid w-full gap-8 pb-16 pt-12 md:gap-16">
+                  <h1 className="grid gap-16 text-5xl font-bold tracking-tight md:text-[4.75rem] lg:text-[4.75rem] text-center">
+                    <span><a className="text-[#000000]">NextJS</a> with <a className="text-[#fc5200]">Loops</a></span>
+                  </h1>
+                  <div className="inline-flex w-full">
+                    <div className="flex w-full flex-col gap-6">
+                      <div>
+
+                        <div
+                          style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            width: "100%",
+                          }}
+                        >
+                          <p
+                            style={{
+                              fontFamily: `'${formStyles.successFont}', sans-serif`,
+                              color: formStyles.successFontColor,
+                              fontSize: `${formStyles.successFontSizePx}px`,
+                            }}
+                          >
+                            {formStyles.successMessage}
+                          </p>
+                        </div>
+                      </div>
+                      <p className="font-normal text-sm text-inherit -mt-1 text-neutral-400 text-center">Built by <a href="https://twitter.com/malthart" className="font-medium underline underline-offset-4">malthart</a>. The source code is available on <a href="https://github.com/malthart/nextjswithloops" className="font-medium underline underline-offset-4">Github</a>.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      );
+    case ERROR:
+      return (
+        <>
+          <SignUpFormError />
+          <BackButton />
+        </>
+      );
+    default:
+      return (
+        <>
+          <div className="flex flex-1 flex-col">
+            <div className="overflow-hidden">
+              <div className="px-4 md:px-8">
+                <div className="mx-auto w-full max-w-6xl">
+                  <div className="grid w-full gap-8 pb-16 pt-12 md:gap-16">
+                    <h1 className="grid gap-16 text-5xl font-bold tracking-tight md:text-[4.75rem] lg:text-[4.75rem] text-center">
+                      <span><a className="text-[#000000]">NextJS</a> with <a className="text-[#fc5200]">Loops</a></span>
+                    </h1>
+                    <div className="inline-flex w-full">
+                      <div className="flex w-full flex-col gap-6">
+                        <div>
+                          <form
+                            onSubmit={handleSubmit}
+                            style={{
+                              display: "flex",
+                              flexDirection: isInline ? "row" : "column",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              width: "100%",
+                            }}
+                          >
+                            <input
+                              type="text"
+                              name="email"
+                              placeholder={formStyles.placeholderText}
+                              value={email}
+                              onChange={(e) => setEmail(e.target.value)}
+                              required={true}
+                              style={{
+                                color: formStyles.formFontColor,
+                                fontFamily: `'${formStyles.formFont}', sans-serif`,
+                                fontSize: `${formStyles.formFontSizePx}px`,
+                                margin: isInline ? "0px 10px 0px 0px" : "0px 0px 10px",
+                                width: "100%",
+                                maxWidth: "300px",
+                                minWidth: "100px",
+                                background: "#FFFFFF",
+                                border: "1px solid #D1D5DB",
+                                boxSizing: "border-box",
+                                boxShadow: "rgba(0, 0, 0, 0.05) 0px 1px 2px",
+                                borderRadius: "6px",
+                                padding: "8px 12px",
+                              }}
+                            />
+                            <SignUpFormButton />
+                          </form>
+                        </div>
+                        <p className="font-normal text-sm text-inherit -mt-1 text-neutral-400 text-center">Built by <a href="https://twitter.com/malthart" className="font-medium underline underline-offset-4">malthart</a>. The source code is available on <a href="https://github.com/malthart/nextjswithloops" className="font-medium underline underline-offset-4">Github</a>.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+  }
+
+  function SignUpFormError() {
+    return (
+      <div
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <p
+          style={{
+            fontFamily: "Inter, sans-serif",
+            color: "rgb(185, 28, 28)",
+            fontSize: "14px",
+          }}
+        >
+          {errorMessage || "Oops! Something went wrong, please try again"}
+        </p>
       </div>
+    );
+  }
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  function BackButton() {
+    const [isHovered, setIsHovered] = useState(false);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    return (
+      <button
+        style={{
+          color: "#6b7280",
+          font: "14px, Inter, sans-serif",
+          margin: "10px auto",
+          textAlign: "center",
+          background: "transparent",
+          border: "none",
+          cursor: "pointer",
+          textDecoration: isHovered ? "underline" : "none",
+        }}
+        onMouseOut={() => setIsHovered(false)}
+        onMouseOver={() => setIsHovered(true)}
+        onClick={resetForm}
+      >
+        &larr; Back
+      </button>
+    );
+  }
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+  function SignUpFormButton({ props }: any) {
+    return (
+      <button
+        type="submit"
+        style={{
+          background: formStyles.buttonColor,
+          fontSize: `${formStyles.buttonFontSizePx}px`,
+          color: formStyles.buttonFontColor,
+          fontFamily: `'${formStyles.buttonFont}', sans-serif`,
+          width: isInline ? "min-content" : "100%",
+          maxWidth: "300px",
+          whiteSpace: isInline ? "nowrap" : "normal",
+          height: "38px",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "row",
+          padding: "9px 17px",
+          boxShadow: "0px 1px 2px rgba(0, 0, 0, 0.05)",
+          borderRadius: "6px",
+          textAlign: "center",
+          fontStyle: "normal",
+          fontWeight: 500,
+          lineHeight: "20px",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        {formState === SUBMITTING ? "Please wait..." : formStyles.buttonText}
+      </button>
+    );
+  }
+}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+function isValidEmail(email: any) {
+  return /.+@.+/.test(email);
 }
