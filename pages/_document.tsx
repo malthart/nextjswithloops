260cbd81cb5121139ac840802ba4cdb3fd1c103e
Malthe Hartmann <malthart@icloud.com>
2023-09-11 12:18:11 +0200

    Vercel@analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
   return (
@@ -13,6 +14,7 @@ export default function Document() {
         key="desc"
       />
       <body>
+        <Analytics />
         <Main />
         <NextScript />
       </body>
