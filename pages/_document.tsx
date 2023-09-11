8ec7c6c2432cf38c846ea0da1799337aecd94164
Malthe Hartmann <malthart@icloud.com>
2023-09-11 11:46:24 +0200

    meta title+description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>
+        NextJS with Loops
+      </title>
+      <meta
+        name="description"
+        content="Lead generation & e-mail automation-ready boilerplate built with NextJS, Tailwind & Loops."
+        key="desc"
+      />
       <body>
         <Main />
         <NextScript />
